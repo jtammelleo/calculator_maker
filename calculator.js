342c7d799085c390cbf70b7f773cbd84509c82fa
jtammelleo <tammelleo@hotmail.com>
2023-01-10 18:04:19 -0500

    Basic 4-function calculator in js

---
+++ calculator.js
@@ -0,0 +1,35 @@
+// Multiply
+function multiply(a,b) { 
+    return a * b
+}
+
+var x = multiply(8,9)
+
+console.log(x)
+
+// Divide
+function divide(a,b) { 
+    return a / b
+}
+
+var y = divide(8,9)
+
+console.log(y)
+
+// Add
+function add(a,b) { 
+    return a + b
+}
+
+var z = add(8,9)
+
+console.log(z)
+
+// Subtract
+function subtract(a,b) { 
+    return a - b
+}
+
+var s = subtract(8,9)
+
+console.log(s)
