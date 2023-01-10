502b17aa9b59adc289f75e5b4635b2ef69041a2d
jtammelleo <tammelleo@hotmail.com>
2023-01-10 17:45:37 -0500

    Fix error with printing for all 4 functions at end

--- calculator.py
+++ calculator.py
@@ -16,16 +16,16 @@ def subtract(a,b):
 
 print("I'm going use the calculator functions to multiply 5 and 6")
 a = multiply(5,6)
-print(x)
+print(a)
 
 print("I'm going use the calculator functions to divide 5 and 6")
 b = divide(5,6)
-print(x)
+print(b)
 
 print("I'm going use the calculator functions to add 5 and 6")
 c = add(5,6)
-print(x)
+print(c)
 
 print("I'm going use the calculator functions to subtract 5 and 6")
 d = subtract(5,6)
-print(x)
+print(d)
