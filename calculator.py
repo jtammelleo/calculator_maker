f5694524eeacdba529c10d66efaf063558d24d80
jtammelleo <tammelleo@hotmail.com>
2023-01-10 17:59:28 -0500

    Print square_n_times at end

--- calculator.py
+++ calculator.py
@@ -26,26 +26,30 @@ def cube(a):
 def sqn(a,b):
 	return a**b
 
-print("I'm going use the calculator functions to multiply 5 and 6")
+print("I'm going to use the calculator functions to multiply 5 and 6")
 m = multiply(5,6)
 print(m)
 
-print("I'm going use the calculator functions to divide 5 and 6")
+print("I'm going to use the calculator functions to divide 5 and 6")
 d = divide(5,6)
 print(d)
 
-print("I'm going use the calculator functions to add 5 and 6")
+print("I'm going to use the calculator functions to add 5 and 6")
 z = add(5,6)
 print(z)
 
-print("I'm going use the calculator functions to subtract 5 and 6")
+print("I'm going to use the calculator functions to subtract 5 and 6")
 s = subtract(5,6)
 print(s)
 
-print("I'm going use the calculator functions to square 5")
+print("I'm going to use the calculator functions to square 5")
 q = square(5)
 print(q)
 
-print("I'm going use the calculator functions to cube 5")
+print("I'm going to use the calculator functions to cube 5")
 c = cube(5)
 print(c)
+
+print("I'm going to use the calculator functions to exponent 5, 6 times")
+e = sqn(5,6)
+print(e)
